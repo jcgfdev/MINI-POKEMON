e56b8512b4dd610149c2312e86cab478fcf5badd
Nestel23 <n.nestel@hotmail.com>
2023-03-21 18:07:14 -0500

    cambios generales

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,87 @@
 Page({
-  onLoad(query) {
-    // Page load
-    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  data:{
+    pokemon:[],
+    list3: [
+      {
+        icon: 'https://img.example.com/example1.png',
+        text: 'Titulo',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example2.png',
+        text: 'Title',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example3.png',
+        text: 'Title',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example4.png',
+        text: 'Title',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example5.png',
+        text: 'Title',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example6.png',
+        text: 'Title',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example7.png',
+        text: 'Title',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example8.png',
+        text: 'Title',
+        desc: 'text',
+      },
+      {
+        icon: 'https://img.example.com/example9.png',
+        text: 'Title',
+        desc: 'text',
+      },
+    ],
+    onItemClick(ev) {
+      my.alert({
+        content: ev.detail.index,
+      });
+    },
   },
-  onReady() {
-    // Page loading is complete
-  },
-  onShow() {
-    // Page display
-  },
-  onHide() {
-    // Page hidden
-  },
-  onUnload() {
-    // Page is closed
-  },
-  onTitleClick() {
-    // Title clicked
-  },
-  onPullDownRefresh() {
-    // Page is pulled down
-  },
-  onReachBottom() {
-    // Page is pulled to the bottom
-  },
-  onShareAppMessage() {
-    // Back to custom sharing information
-    return {
-      title: 'My App',
-      desc: 'My App description',
-      path: 'pages/index/index',
-    };
-  },
+  
+  
+  onLoad() {
+      this.servicioGet();
+    },
+      servicioGet(){
+      let prueba;
+      my.httpRequest({
+        url: 'https://pokeapi.co/api/v2/pokemon-species?offset=0&limit=4',        
+      }).then((res2) => {   
+        prueba = res2.data.results;    
+
+        let arr = Array();
+      prueba.forEach(item => {
+        arr.push({
+          nombre:item.name      
+         
+        });        
+      });    
+       
+        this.setData({pokemon:arr}); 
+
+        console.log(this.data.list3);
+        console.log(this.data.pokemon);
+      
+        },(res) => {     
+        console.log(res.error, res.errorMessage);
+      })
+    }
+ 
 });
