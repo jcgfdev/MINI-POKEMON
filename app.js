46de83a781ce60b3c2ed65a5c65ec05174c14b8d
Nestel23 <n.nestel@hotmail.com>
2023-03-21 16:45:36 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+App({
+  onLaunch(options) {
+    // Page opens for the first time
+    console.info('App onLaunch');
+  },
+  onShow(options) {
+    // Reopened by scheme from the background
+  },
+});
---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+Page({
+  onLoad(query) {
+    // Page load
+    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  },
+  onReady() {
+    // Page loading is complete
+  },
+  onShow() {
+    // Page display
+  },
+  onHide() {
+    // Page hidden
+  },
+  onUnload() {
+    // Page is closed
+  },
+  onTitleClick() {
+    // Title clicked
+  },
+  onPullDownRefresh() {
+    // Page is pulled down
+  },
+  onReachBottom() {
+    // Page is pulled to the bottom
+  },
+  onShareAppMessage() {
+    // Back to custom sharing information
+    return {
+      title: 'My App',
+      desc: 'My App description',
+      path: 'pages/index/index',
+    };
+  },
+});
