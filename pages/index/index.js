3976845d36e256cefee81a579bade780926291ab
Nestel23 <n.nestel@hotmail.com>
2023-03-23 15:39:03 -0500

    cambios funciona

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,48 @@
 Page({
-      data: {
-        pokemon: [],
-        colorP: ""
-      },
+  data: {
+    pokemon: [],   
+  },
 
-      async servicioColr(id) {
-        const response = await my.request({
-          url: `https://pokeapi.co/api/v2/pokemon-species/${id}/`,
-          method: 'GET',
-          dataType: 'json',
-          success: function (res) {              
-            this.setData({colorP:res.data.color.name})  
-            console.log(this.data.colorP)        
-            // return res.data.color.name;
-          }
-      })
-
-    
+  async servicioColr(id) {
+    try {
+      const response = await my.request({
+        url: `https://pokeapi.co/api/v2/pokemon-species/${id}/`,
+        method: 'GET',
+        dataType: 'json',
+      });
+      return response.data.color.name;
+    } catch (error) {
+      console.error(error);
+    }
   },
 
   onLoad() {
     this.servicioGet();
-    // console.log(await this.servicioColr(1));
   },
 
-
-
-  async servicioGet() {
-      let prueba;
-      my.httpRequest({
-        url: 'https://pokeapi.co/api/v2/pokemon-species?offset=0&limit=105',
-      }).then((res2) => {
-        prueba = res2.data.results;
-        this.servicioColr(1)
-        console.log(this.data.colorP)
-        let arr = Array();
-        prueba.forEach(item => {
-          let aux = item.url.split('/');
-
-
+  servicioGet() {
+    let prueba;
+    my.httpRequest({
+      url: 'https://pokeapi.co/api/v2/pokemon-species?offset=0&limit=5',
+    }).then((res2) => {
+      prueba = res2.data.results;
+      let arr = Array();
+      prueba.forEach(item => {
+        let aux = item.url.split('/');
+        this.servicioColr(aux[6]).then(res => {
           arr.push({
             nombre: item.name,
             imagen: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${aux[6]}.svg`,
-            color: ""
+            color: res
           });
-        });
-
-        this.setData({
-          pokemon: arr
-        });
-
-        // console.log(this.data.colorPokemon)
-        console.log(this.data.pokemon);
-        // console.log(this.data.list3);
-
-      }, (res) => {
-        console.log(res.error, res.errorMessage);
-      })
-    },
-
-
-
-
+        })
+      });
+      this.setData({
+        pokemon: arr
+      });
+      console.log(this.data.pokemon);
+    }, (res) => {
+      console.log(res.error, res.errorMessage);
+    })
+  },
 });
